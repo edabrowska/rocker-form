108fbc74cd85093e3f588d9221a9291b225835ad
Edyta Dąbrowska <editt.dabrowska@gmail.com>
2020-03-13 14:00:49 +0100

    add docks folder to create github page

---
+++ docks/precache-manifest.9e48c6ffa200857653173d2d468d0cbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0697b083b65ae8687a9fdc41c6d806e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e18358a584cfb0677323",
+    "url": "/static/js/2.6fe90e31.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.6fe90e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d132b5a3e94336a67a",
+    "url": "/static/js/main.06360fed.chunk.js"
+  },
+  {
+    "revision": "76ffbcc00adf60da3579",
+    "url": "/static/js/runtime-main.fca3c6b0.js"
+  }
+]);
